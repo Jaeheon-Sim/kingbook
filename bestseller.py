f508c36c1bfca76a83de750e5362a5c393c1f80f
늇뉴진 <68093782+zladb@users.noreply.github.com>
2021-12-18 04:15:20 +0900

    랭킹 날짜 자동 갱신
    집계 첫날 기준으로 데이터 베이스에 저장

--- bestseller.py
+++ bestseller.py
@@ -3,25 +3,30 @@
 # 교보문고의 주간베스트 20의 데이터를 크롤링해와서 파이어베이스에 저장하는 프로그램.
 
 # -*- coding: utf-8 -*-
-
-from urllib.request import urlopen
+import re
+import urllib
+from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
-import firebase_admin
-
-from firebase_admin import credentials
 from firebase_admin import db
+from firebase_admin import credentials, initialize_app, storage
+
 
 # 파이어베이스 주소 저장
-db_url = 'https://kingbookdb-default-rtdb.asia-southeast1.firebasedatabase.app/'
+db_url = 'https://helpme-378f8-default-rtdb.firebaseio.com/'
+storage_url = 'helpme-378f8.appspot.com'
 
 # 파이어베이스 연결 (비공개 키 + 파이어베이스 주소)
-cred = credentials.Certificate("kingbookdb-firebase-adminsdk-e89jh-6191d9d721.json")
-default_app = firebase_admin.initialize_app(cred, {'databaseURL': db_url})
-
+cred = credentials.Certificate("helpme-378f8-firebase-adminsdk-dwsva-df6ce4c883.json")
+initialize_app(cred, {'databaseURL': db_url, 'storageBucket': storage_url})
 
 # 교보문고의 베스트셀러 웹페이지를 가져옵니다.
 html = urlopen('http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf')
 soup = BeautifulSoup(html, "html.parser")
+
+date = soup.find('div', "content_middle").find('h4', "title_best_basic").find('small').text.strip()
+sep_date = re.findall("\d+", date)
+mmdd = sep_date[1]+sep_date[2]
+# print(mmdd)
 
 # 책의 상세 웹페이지 주소를 추출하여 리스트에 저장합니다.
 book_page_urls = []
@@ -75,6 +80,28 @@ for index, book_page_url in enumerate(book_page_urls):
     # 책 링크
     url = soup.find('meta', {'property': 'og:url'}).get('content')
 
+    # 이미지 웹에서 다운로드해서 저장하기
+    imgURL = str(image)
+    imagePath = "D:/bookcrawling/{0}.jpg".format(ISBN13)
+
+    urllib.request.urlretrieve(imgURL, imagePath)
+
+    # 기본 버킷 사용
+    bucket = storage.bucket()
+
+    # 경로 지정
+    blob = bucket.blob('bookImages/'+str(ISBN13))
+    
+    # 메타데이터에 책 
+    metadata = {"book_title": title}
+    blob.metadata = metadata
+
+    # 파이어베이스 파일 업로드
+    blob.upload_from_filename(filename=imagePath, content_type='image/jpeg')
+
+    blob.make_public()
+    print("path:" + blob.public_url)
+
     # 딕셔너리 형식으로 데이터 저장
     book_info = {
         'title': title,
@@ -90,7 +117,9 @@ for index, book_page_url in enumerate(book_page_urls):
         'title': title,
         'author': author,
         'rank': rank,
-        'ISBN': ISBN13
+        'ISBN': ISBN13,
+        'tag_list': tag,
+        'image': str(blob.public_url)
     }
 
     # 파이어베이스에 데이터 삽입하는 부분
@@ -99,8 +128,9 @@ for index, book_page_url in enumerate(book_page_urls):
     book_ref.set(book_info)
 
     # 데이터를 끌어온 날짜(mmdd)의 랭킹 정보를 저장.
-    rank_ref = db.reference('RANK/%d/%d' % (1115, index))
+    rank_ref = db.reference('RANK/%s/%d' % (mmdd, index))
     rank_ref.set(rank_info)
+
 
     print(rank)
     print(title)
